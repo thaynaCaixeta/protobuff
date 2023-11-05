ba22a5b5f4fce94e9473b757896df9f6d3b4f4b5
Thayna Caixeta <thayna.caixeta@gmail.com>
2023-11-05 23:58:11 +0000

    Enums with Protobuf

--- src/main/java/com/tackr/protobuf/MapDemo.java
+++ src/main/java/com/tackr/protobuf/MapDemo.java
@@ -1,5 +1,6 @@
 package com.tackr.protobuf;
 
+import com.tackr.protobuf.model.BodyStyle;
 import com.tackr.protobuf.model.Car;
 import com.tackr.protobuf.model.Dealer;
 
@@ -11,12 +12,14 @@ public class MapDemo {
                 .setMake("Ford")
                 .setModel("Mustang")
                 .setYear(2023)
+                .setBodyStyle(BodyStyle.SPORTIVE)
                 .build();
 
         Car civic = Car.newBuilder()
                 .setMake("Honda")
                 .setModel("Civic")
                 .setYear(2021)
+                .setBodyStyle(BodyStyle.SEDAN)
                 .build();
 
         Dealer dealer = Dealer.newBuilder()
@@ -24,6 +27,6 @@ public class MapDemo {
                 .putModel(2021, civic)
                 .build();
 
-        System.out.println(dealer.getModelOrDefault(2023, civic));
+        System.out.println(dealer.getModelOrThrow(2023).getBodyStyle());
     }
 }
